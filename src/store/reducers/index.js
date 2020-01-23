ac50824ae2b41ce1b7e69ce7ada5b70527b4a4f3
AlexanderPugachev <al.pugachev96@gmail.com>
2020-01-23 20:14:15 +0300

    fix bug with redux-form extention

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
 import works from './works'
 
 // для подключения новых редьюсеров импортировать и передать в combineReducers
 
 export default combineReducers({
-	works,
-	form: formReducer
+	works
 })
